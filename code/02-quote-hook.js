a0477c6c8fe944d83c76d1b405de73302122b3e5
Steven Sajja <stevensajja@Gmail.com>
2023-11-30 19:06:47 +0200

    Update 02-quote-hook.js

--- code/02-quote-hook.js
+++ code/02-quote-hook.js
@@ -8,26 +8,30 @@
  * @see {@link https://docs.rootplatform.com/docs/quote-hook Quote hook}
  */
 const validateQuoteRequest = (data) => {
-  const species = [
-    'Tyrannosaurus Rex',
-    'Stegosaurus',
-    'Velociraptor',
-    'Diplodocus',
-    'Iguanodon',
-  ];
   const validationResult = Joi.validate(
     data,
     Joi.object()
       .keys({
-        // keys and validation
-        type: Joi.valid(species).required(),
-        start_date: Joi.date().iso().min('now').max('60 days').required(),
+        species: Joi.valid([
+          'Tyrannosaurus Rex',
+          'Stegosaurus',
+          'Velociraptor',
+          'Diplodocus',
+          'Iguanodon',
+        ]).required(),
+        start_date: Joi.date()
+          .min(moment().format('YYYY-MM-DD'))
+          .max(moment().add(60, 'days').format('YYYY-MM-DD'))
+          .required(),
         cover_amount: Joi.number()
           .integer()
-          .min(10000.0)
-          .max(100000.0)
+          .min(10000 * 100)
+          .max(100000 * 100)
           .required(),
-        birth_date: Joi.date().iso().max('now').min('50 years').required(),
+        birth_date: Joi.number()
+          .integer()
+          .max(moment().year() - 50)
+          .required(),
         health_checks_updated: Joi.boolean().required(),
       })
       .required(),
@@ -43,41 +47,43 @@ const validateQuoteRequest = (data) => {
  *     [Create a quote](https://docs.rootplatform.com/reference/getting-a-quote-2) endpoint.
  * @see {@link https://docs.rootplatform.com/docs/quote-hook Quote hook}
  */
+
 const getQuote = (data) => {
-  let corePremium = data.cover_amount * (0.01 * (data.age * 0.001));
-  let typeBasedPremium = corePremium;
-  if (data.type === 'Tyrannosaurus Rex') {
-    typeBasedPremium = 0.81 * corePremium;
-  } else if (data.type === 'Stegosaurus') {
-    typeBasedPremium = 1.19 * corePremium;
-  } else if (data.type === 'Velociraptor') {
-    typeBasedPremium = 0.76 * corePremium;
-  } else if (data.type === 'Brachiosaurus') {
-    typeBasedPremium = 1.32 * corePremium;
-  } else if (data.type === 'Iguanodon') {
-    typeBasedPremium = 1.07 * corePremium;
+  const age = moment().year() - data.birth_date;
+  let corePremium = data.cover_amount * (0.01 * (age * 0.001));
+  if (data.species === 'Tyrannosaurus Rex') {
+    corePremium = 0.81 * corePremium;
+  } else if (data.species === 'Stegosaurus') {
+    corePremium = 1.19 * corePremium;
+  } else if (data.species === 'Velociraptor') {
+    corePremium = 0.76 * corePremium;
+  } else if (data.species === 'Brachiosaurus') {
+    corePremium = 1.32 * corePremium;
+  } else if (data.species === 'Iguanodon') {
+    corePremium = 1.07 * corePremium;
   }
 
   if (!data.health_checks_updated) {
-    typeBasedPremium += 25000;
-    corePremium += 25000;
+    corePremium = corePremium + 2500;
   }
+
+  const premium = Math.round(corePremium);
 
   const quotePackage = new QuotePackage({
     // Below are standard fields for all products
     package_name: 'DinoSure Protection', // The name of the "package" of cover
-    // sum_assured: 10000 * 100, // Set the total, aggregated cover amount
     sum_assured: data.cover_amount, // Set the total, aggregated cover amount
-    base_premium: corePremium, // Should be an integer, cents
-    suggested_premium: typeBasedPremium, // Should be an integer, cents
+    base_premium: premium, // Should be an integer, cents
+    suggested_premium: premium, // Should be an integer, cents
     billing_frequency: 'monthly', // Can be monthly or yearly
     module: {
       // Save any data, calculations, or results here for future re-use.
-      type: 'dinosure_steven_ext_dev',
-      start_date: '2023-02-03T11:17:09.747Z',
-      cover_amount: 10000,
-      birth_date: '2020-02-03T11:17:09.747Z',
-      health_checks_updated: true,
+      type: 'dinosure_course_steven',
+      species: data.species,
+      start_date: data.start_date,
+      cover_amount: data.cover_amount,
+      birth_date: data.birth_date,
+      health_checks_updated: data.health_checks_updated,
       ...data,
     },
     input_data: { ...data },
