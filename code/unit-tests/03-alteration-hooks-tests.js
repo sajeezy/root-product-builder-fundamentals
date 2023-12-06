44aefaa601b54cbe11cdec6c4576bc0aed63595c
Steven Sajja <stevensajja@Gmail.com>
2023-12-06 17:43:30 +0200

    Create unit tests that thoroughly test validation

--- code/unit-tests/03-alteration-hooks-tests.js
+++ code/unit-tests/03-alteration-hooks-tests.js
@@ -4,9 +4,10 @@
 // This ensures that it does not interfere with production execution.
 
 describe('Amendment alteration hook', function () {
-  const alterationHookKey = '<ALTERATION HOOK KEY>';
+  const alterationHookKey = 'modify_cover_amount';
 
-  it('valid data should pass validation', function () {
+  // A payload with valid data passes validateAlterationPackageRequest
+  it('valid data  should pass validation', function () {
     const validationResult = validateAlterationPackageRequest({
       alteration_hook_key: alterationHookKey,
       data: validAlterationData,
@@ -16,26 +17,43 @@ describe('Amendment alteration hook', function () {
     expect(validationResult.error).to.equal(null);
   });
 
-  it('invalid pet name should generate validation error', function () {
+  // A payload with invalid data fails validateAlterationPackageRequest
+  it('invalid data should generate validation error', function () {
     const validationResult = validateAlterationPackageRequest({
       alteration_hook_key: alterationHookKey,
       data: invalidAlterationData,
       policy: undefined,
       policyholder: undefined,
     });
-    expect(validationResult.error.message).to.equal(
-      `<COPY JOI ERROR MESSAGE HERE>`,
-    );
+    expect(validationResult.error).to.not.equal(null);
   });
 
-  it('should update the <PROPERTY> to "<PROPERTY>"', function () {
+  // A 20-year-old Tyrannosaurus Rex with R90,000.00 has its cover changed to R75,000.00 with a resulting premium of R1215.00
+  it('should update the cover amount and premium for a 20-year-old Tyrannosaurus Rex', function () {
     const alterationPackage = getAlteration({
       alteration_hook_key: alterationHookKey,
       data: validAlterationData,
-      // @ts-ignore
+      //@ts-ignore
+      policy: {
+        ...examplePolicy,
+        monthly_premium: 121500,
+      },
+      policyholder: undefined,
+    });
+    expect(alterationPackage.module.cover_amount).to.equal(7500000);
+    expect(alterationPackage.monthly_premium).to.equal(121500);
+  });
+
+  // A 36-year-old Velociraptor with R50,000.00 has its cover changed to R75,000.00 with a resulting premium of R2052.00
+  it('should update the cover amount and premium for a 36-year-old Velociraptor', function () {
+    const alterationPackage = getAlteration({
+      alteration_hook_key: alterationHookKey,
+      data: validAlterationData,
+      //@ts-ignore
       policy: examplePolicy,
       policyholder: undefined,
     });
-    expect(alterationPackage.module.SOME_PROPERTY).to.equal('<SOME PROPERTY>');
+    expect(alterationPackage.module.cover_amount).to.equal(7500000);
+    expect(alterationPackage.monthly_premium).to.equal(205200);
   });
 });
