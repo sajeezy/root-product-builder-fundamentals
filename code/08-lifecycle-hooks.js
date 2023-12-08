be6ecd0ed52dd620cd99590dea5140cf57c4ab65
Steven Sajja <stevensajja@Gmail.com>
2023-12-08 08:25:13 +0200

    lifecycle hooks

--- code/08-lifecycle-hooks.js
+++ code/08-lifecycle-hooks.js
@@ -1,1 +1,82 @@
 // 08-lifecycle-hooks
+
+/**
+ * Get the reactivation options for inactive policies.
+ * @param {PlatformPolicy} policy The policy to be reactivated.
+ * @return {ReactivationOption[]} One of these options must be selected whenever an inactive policy is reactivated.
+ */
+const getReactivationOptions = (policy) => {
+  const settlementAmount = policy.balance < 0 ? -policy.balance : 0;
+  return [
+    new ReactivationOption({
+      type: 'reinstatement',
+      settlementAmount,
+      description:
+        'For a policy to be reinstated, all arrear premiums will immediately be due.',
+      minimumBalanceRequired: true,
+    }),
+    new ReactivationOption({
+      type: 'recommencement',
+      description:
+        'For a policy to be recommenced, all arrear premiums will be deducted from the first claim income.',
+      minimumBalanceRequired: false,
+    }),
+  ];
+};
+
+/**
+ * Executed before a policy is reactivated.
+ * Can be used to prevent reactivation if certain conditions are not met.
+ * @param {object} params
+ * @param {PlatformPolicy} params.policy The policy to be reactivated
+ * @param {PlatformPolicyholder} params.policyholder The policyholder linked to the policy
+ * @param {ReactivationOption} params.reactivationOption The reactivation option selected
+ * @return {ProductModuleAction[] | void} The actions to be queued by the platform
+ */
+const beforePolicyReactivated = ({
+  policy,
+  policyholder,
+  reactivationOption,
+}) => {
+  // Check policy status is lapsed or cancelled
+  const isPolicyLapsedOrCancelled = ['lapsed', 'cancelled'].includes(
+    policy.status,
+  );
+  if (!isPolicyLapsedOrCancelled) {
+    throw new Error(
+      `Policy with status ${policy.status} cannot be reactivated. Policy status must be one of ['lapsed', 'cancelled'].`,
+    );
+  }
+
+  // Check policy was active within the last six months
+  const sixMonthsFromUpdated = moment(policy.status_updated_at).add(
+    6,
+    'months',
+  );
+  if (isPolicyLapsedOrCancelled && moment().isAfter(sixMonthsFromUpdated)) {
+    throw new Error(
+      `Policy can only be reactivated within 6 months of lapse or cancellation. Latest reactivation date was ${sixMonthsFromUpdated.format(
+        'YYYY-MM-DD',
+      )}`,
+    );
+  }
+  setReactivationDate({ policy, reactivation_date: moment() });
+};
+
+const setReactivationDate = ({ policy, reactivation_date }) => {
+  const newModule = {
+    ...policy.module,
+    start_date: reactivation_date,
+  };
+
+  console.log('newModule', newModule);
+  return [
+    {
+      // name: 'activate_policy',
+      name: 'update_policy',
+      data: {
+        module: newModule,
+      },
+    },
+  ];
+};
---
+++ code/unit-tests/04-lifecycle-hooks.tests.js
@@ -0,0 +1,106 @@
+const policy_active = {
+  policy_number: generatePolicyNumber(),
+  package_name: 'DinoSure Protection', // The name of the "package" of cover
+  sum_assured: 7500000,
+  base_premium: 10000,
+  monthly_premium: 205200,
+  start_date: moment().add(1, 'day').format(), // policy starts the day after issue
+  end_date: null,
+};
+const policy_lapsed = {
+  ...policy_active,
+  status: 'lapsed',
+};
+const policy_cancelled = {
+  ...policy_active,
+  status: 'cancelled',
+};
+const policy_expired = {
+  ...policy_active,
+  status: 'expired',
+};
+
+describe('Reactivation flow', () => {
+  // Setup
+  let reactivationOption;
+
+  before(() => {
+    // @ts-ignore
+    reactivationOption = getReactivationOptions(policy_active)[0];
+  });
+
+  // Quote hook
+  describe('Before reactivation', () => {
+    //   it('should return an error because the policy is already active', async () => {
+    //     var error = null;
+    //     try {
+    //       await beforePolicyReactivated({
+    //         // @ts-ignore
+    //         policy: policy_active,
+    //         reactivationOption: reactivationOption,
+    //       });
+    //     } catch (e) {
+    //       error = e;
+    //     }
+    //     expect(error).to.not.equal(null);
+    //   });
+
+    //   it('should return without error: lapsed policy', () => {
+    //     var error = null;
+    //     try {
+    //       beforePolicyReactivated({
+    //         // @ts-ignore
+    //         policy: policy_lapsed,
+    //         reactivationOption: reactivationOption,
+    //       });
+    //     } catch (e) {
+    //       error = e;
+    //     }
+    //     expect(error).to.equal(null);
+    //   });
+
+    //   it('should return without error: cancelled policy', () => {
+    //     var error = null;
+    //     try {
+    //       beforePolicyReactivated({
+    //         // @ts-ignore
+    //         policy: policy_cancelled,
+    //         reactivationOption: reactivationOption,
+    //       });
+    //     } catch (e) {
+    //       error = e;
+    //     }
+    //     expect(error).to.equal(null);
+    //   });
+
+    // A policy being reactivated with a status of cancelled and lapsed can be reactivated.
+    it('cancelled policy can be reactivated', () => {
+      var error = null;
+      try {
+        beforePolicyReactivated({
+          // @ts-ignore
+          policy: policy_cancelled || policy_lapsed,
+          reactivationOption: reactivationOption,
+        });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).to.equal(null);
+    });
+
+    // A policy with a status of expired cannot be reactivated.
+    it('expired policy cannot be reactivated', () => {
+      var error = null;
+      try {
+        beforePolicyReactivated({
+          // @ts-ignore
+          policy: policy_expired,
+          reactivationOption: reactivationOption,
+        });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).to.not.equal(null);
+    });
+  });
+});
