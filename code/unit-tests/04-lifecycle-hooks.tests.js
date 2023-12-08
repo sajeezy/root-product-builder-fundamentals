bc92431a80a709894f9437a1eeb62933d675a440
Steven Sajja <stevensajja@Gmail.com>
2023-12-08 09:04:06 +0200

    Update 04-lifecycle-hooks.tests.js

--- code/unit-tests/04-lifecycle-hooks.tests.js
+++ code/unit-tests/04-lifecycle-hooks.tests.js
@@ -31,48 +31,6 @@ describe('Reactivation flow', () => {
 
   // Quote hook
   describe('Before reactivation', () => {
-    //   it('should return an error because the policy is already active', async () => {
-    //     var error = null;
-    //     try {
-    //       await beforePolicyReactivated({
-    //         // @ts-ignore
-    //         policy: policy_active,
-    //         reactivationOption: reactivationOption,
-    //       });
-    //     } catch (e) {
-    //       error = e;
-    //     }
-    //     expect(error).to.not.equal(null);
-    //   });
-
-    //   it('should return without error: lapsed policy', () => {
-    //     var error = null;
-    //     try {
-    //       beforePolicyReactivated({
-    //         // @ts-ignore
-    //         policy: policy_lapsed,
-    //         reactivationOption: reactivationOption,
-    //       });
-    //     } catch (e) {
-    //       error = e;
-    //     }
-    //     expect(error).to.equal(null);
-    //   });
-
-    //   it('should return without error: cancelled policy', () => {
-    //     var error = null;
-    //     try {
-    //       beforePolicyReactivated({
-    //         // @ts-ignore
-    //         policy: policy_cancelled,
-    //         reactivationOption: reactivationOption,
-    //       });
-    //     } catch (e) {
-    //       error = e;
-    //     }
-    //     expect(error).to.equal(null);
-    //   });
-
     // A policy being reactivated with a status of cancelled and lapsed can be reactivated.
     it('cancelled policy can be reactivated', () => {
       var error = null;
